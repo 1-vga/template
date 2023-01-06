ce4662bdcb12803776fa056a2f8af7f5a172af1e
Andrii <66524005+AndriiLutsiv@users.noreply.github.com>
2023-01-06 18:08:21 +0200

    pagination started

--- src/pages/labs-page/labs-page.tsx
+++ src/pages/labs-page/labs-page.tsx
@@ -1,21 +1,21 @@
 
 import React, { useState } from 'react';
 import styles from './labs-page.module.scss';
-
 import Subscribe from '../../components/subscribe/subscribe'
-import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { Search } from '../../components/search';
-
 import { CardLab } from '../../components/card-lab';
 import photo from './images/roundedRectangle.jpg'
 import photoTwo from "./images/roundedRectangleTwo.jpg";
 import photoThree from "./images/roundedRectangleThree.jpg";
 import { Filters } from '../../components/filters';
+import { Pagination } from '../../components/pagination';
 
 interface Props {
 }
 
 const LabsPage: React.FC<Props> = (props) => {
+    const [currentPage, setCurrentPage] = useState(1);
+
     return <div className={styles.labsPage}>
         <section className={styles.sectionTop}>
             <Search title='labs' />
@@ -30,6 +30,7 @@ const LabsPage: React.FC<Props> = (props) => {
                     <CardLab photo={photo} name='EUROFARM' location='Ozrakovići bb' time='Mon-Sun: 10:00 - 18:00' tell='+387 32 732 100' website='eurofarm.ba' />
                     <CardLab photo={photoThree} name='ORTHOS' location='Ozrakovići bb' time='Mon-Sun: 10:00 - 18:00' tell='+387 32 732 100' website='eurofarm.ba' />
                 </div>
+                <Pagination itemsPerPage={1}/>
             </div>
         </section>
         <Subscribe />
