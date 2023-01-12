0c8c53b6dc1623041b5333309beedd6f7d69c8fb
Andrii <66524005+AndriiLutsiv@users.noreply.github.com>
2023-01-12 15:27:15 +0200

    update

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import styles from './upload.module.scss';
-import avatar from './images/photo.png';
+import avatar from './images/avatar.svg';
 import uploadImg from './images/uploadImg.svg';
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
 
 }
 const Upload: React.FC<Props> = () => {
 
-    const [file, setFile] = React.useState('');
+    const [file, setFile] = React.useState<{
+        id: string;
+        path: string;
+        file: File;
+    } | null>(null);
     const [text, setText] = React.useState('');
-    
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const fileContent = event.target.files;
+        if (fileContent) {
+            const preparedFile = {
+                id: uuidv4(),
+                path: URL.createObjectURL(fileContent[0]),
+                file: fileContent[0],
+            };
+            setFile(preparedFile);
+        }
+    }
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
@@ -39,29 +55,25 @@ const Upload: React.FC<Props> = () => {
                 <input
                     type="file" multiple
                     className={styles.inputField}
-                    value={file}
-                    onChange={(e) => setFile(e.target.value)}
+                    onChange={handleFileChange}
                 />
                 <div className={styles.labelFile}>
                     <div className={styles.fileTitle}>Upload document</div>
                     <div className={styles.fileImg}><img src={uploadImg} alt="uploadImg" /></div>
                 </div>
             </label>
-            <textarea className={styles.textareaForm} placeholder="Leave a message..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            ></textarea>
+            <textarea
+                className={styles.textareaForm}
+                placeholder="Leave a message..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
             <div className={styles.btnRow}>
                 <div className={styles.btnLeft}> <button type='submit' className={styles.subscribeBtn}>Upload test result</button></div>
                 <div className={styles.btnRigt}> <button type='submit' className={styles.subscribeBtn}>Cancel </button></div>
             </div>
         </form>
     </div>
-
-
-
-
-
 }
 
 export default Upload;
